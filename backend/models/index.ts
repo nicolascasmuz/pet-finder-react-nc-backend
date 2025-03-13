2d088e0606e9661a8ed0c2b9375fa7709abfe6fa
Nicolás Casmuz <nicolascasmuz@gmail.com>
2025-03-13 12:54:09 -0300

    commit-003

--- backend/models/index.ts
+++ backend/models/index.ts
@@ -1,5 +1,13 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(process.env.ELEPHANTSQL_URL);
+const sequelize = new Sequelize(process.env.ELEPHANTSQL_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Necesario para conexiones SSL con Neon
+    },
+  },
+});
 
 export { sequelize };
